9ac2d087310dcb1fcb3905e670e8b9751b3a2690
Mahmoud Saaed Romeh <romeheng1@gmail.com>
2026-06-06 00:39:09 +0300

    Handell Enviroment

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+       
+
+    <div className="min-h-screen flex justify-center items-center">
+      <button className="btn btn-primary">
+        DaisyUI Working 🚀
+      </button>
+    </div>
+
+
+
+      
+          
+            
+       
+    </>  )
+}
+
+export default App
